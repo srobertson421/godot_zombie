677957a81838fc7873c4c5699a820eb47c5f4329
srobertson421 <srobertson421@gmail.com>
2021-12-27 23:36:22 -0800

    initial commit. Added player with movement and bullets

---
+++ Entities/Player/Player.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+export (PackedScene) var Bullet
+
+export var speed = 150
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("shoot"):
+		var bullet = Bullet.instance()
+		owner.add_child(bullet)
+		bullet.scale = Vector2(-1, 1)
+		bullet.transform = $HandgunPosition.global_transform
